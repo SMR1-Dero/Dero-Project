65e4380bda3ab733b85458916ccded50d05b4a11
jochemvlieg <60709461+jochemvlieg@users.noreply.github.com>
2023-04-05 16:11:17 +0200

    Back to Update from 4 april

--- HMI/Vegetables_V4.py
+++ HMI/Vegetables_V4.py
@@ -2,25 +2,20 @@ import cv2
 import pyrealsense2 as rs
 import numpy as np
 import copy
-
 def nothing(x):
     pass
 def getframe(pipeline,crop=((0),(720),(0),(1280))):
     # Wait for a new set of frames from the pipeline
     frames = pipeline.wait_for_frames()
-
     # Retrieve the depth and color frames
     depth_frame_full = frames.get_depth_frame()
     color_frame_full = frames.get_color_frame()
-
     # Convert the frames to numpy arrays
     depth_frame_np = copy.deepcopy(np.asanyarray(depth_frame_full.get_data()))
     color_frame_np = copy.deepcopy(np.asanyarray(color_frame_full.get_data()))
-
     # Crop the frames
     depth_frame_cut = depth_frame_np[crop[0]:crop[1],crop[2]:crop[3]]
     color_frame_cut = color_frame_np[crop[0]:crop[1],crop[2]:crop[3]]
-
     # Return the cropped depth and color frames, as well as the full color frame
     return depth_frame_cut, color_frame_cut, np.asanyarray(color_frame_full.get_data())
 def makeframe():
@@ -58,7 +53,7 @@ def image_edits(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hs
     #find the contours
     (cnts, _) = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return gray,cnts
-def getpoint_round(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
+def getpoint_round(depth_frame,color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
     coordinates=[]
     pointi=None
     gray,cnts=image_edits(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3)#0,80,80,255,255,255
@@ -73,9 +68,8 @@ def getpoint_round(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2
             cv2.circle(color_frame,(i[0],i[1]),2,(0,0,255),3)
             pointi=[(i[0]),(i[1])]
             coordinates.append([(pointi)])
-
-    return color_frame,coordinates,gray,-90,30
-def getpoint_notround_withstem(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
+    return color_frame,coordinates,gray
+def getpoint_notround_withstem(depth_frame,color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
     coordinates=[]
     cx=0
     cy=0
@@ -83,11 +77,11 @@ def getpoint_notround_withstem(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvuppe
     loc1=[]
     pointi=(10,10)
     mask,cnts=image_edits(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3)#0,80,30,255,255,255
-    mask2,cnts2=image_edits(color_frame,25,70,50,35,200,255)#10,60,0,35,200,255
+    mask2,cnts2=image_edits(color_frame,10,60,0,35,200,255)#10,60,0,35,200,255
     for i in cnts2:
         area= cv2.contourArea(i)
         #print ("Area",area)
-        if area>100:
+        if area>20:
             M = cv2.moments(i)
             # Calculate the moments
             if M['m00'] != 0:
@@ -126,7 +120,6 @@ def getpoint_notround_withstem(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvuppe
                         epsilon = 0.005 * cv2.arcLength(c, True)
                         approx = cv2.approxPolyDP(c, epsilon, True)
                         cv2.drawContours(color_frame, [approx], -1, (0, 255, 0), 4)
-
                 if (loc1==[]):
                     cv2.circle(color_frame, (cx, cy), 7, (0, 0, 255), -1)
                     pointi=(cx),(cy)
@@ -135,19 +128,16 @@ def getpoint_notround_withstem(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvuppe
                     epsilon = 0.005 * cv2.arcLength(c, True)
                     approx = cv2.approxPolyDP(c, epsilon, True)
                     cv2.drawContours(color_frame, [approx], -1, (0, 255, 0), 4)
-
-    return color_frame,coordinates,mask2,-90,30
-def getpoint_notround(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
+    return color_frame,coordinates,mask2
+def getpoint_notround(depth_frame,color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3,min_size,max_size):
     coordinates=[]
     cx=0
     cy=0
-    angle=-90.0
-    length = 0.0
     pointi=(10,10)
     mask,cnts=image_edits(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupper2,hsvupper3)
     for c in cnts:
         area= cv2.contourArea(c)
-        if area>200:
+        if area>1000:
             M = cv2.moments(c)
             #print ("Area=",area)
             # Calculate the moments
@@ -166,7 +156,6 @@ def getpoint_notround(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupp
                 approx = cv2.approxPolyDP(c, epsilon, True)
                 cv2.drawContours(color_frame, [approx], -1, (0, 255, 0), 4)
                 
-
                 # cv2.putText(color_frame, f'{length}', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 1)
                 # Calculate the orientation of the contour
                 angle = 0.5 * np.arctan2(2*M['mu11'], M['mu20']-M['mu02'])
@@ -182,22 +171,13 @@ def getpoint_notround(color_frame,hsvunder1,hsvunder2,hsvunder3,hsvupper1,hsvupp
                 
                 # Draw the line
                 cv2.line(color_frame, (x1, y1), (x2, y2), (255, 0, 255), 2)
-                
-                # Draw a line through each contour and print its length
-                x,y,w,h = cv2.boundingRect(c)
-                #cv2.line(color_frame, (x,y), (x+w,y+h), (0,255,0), 2)
-                length = cv2.arcLength(c, True)
-                cv2.putText(color_frame, f'Length:{length:.2f}', (x1-20, y1-50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
-                
-                cv2.putText(color_frame, f'Angle:{np.rad2deg(angle):.2f}', (x1-20,y1-30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 1)
-    return color_frame,coordinates,mask,angle,length
-
+                cv2.putText(color_frame, f'{np.rad2deg(angle)}', (x1,y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,255), 1)
+    return color_frame,coordinates,mask
 def getpoint(pipeline1,pipeline2, vegetable):
     '''
     Description
     ----------
     Function which finds de pickup points for vegetables.
-
     Parameters
     ----------
     pipeline1 : pyrealsense.pipeline
@@ -206,7 +186,6 @@ def getpoint(pipeline1,pipeline2, vegetable):
         Camera pipeline object of camera 2
     vegetable : dictionary
         Dictionary containing all necessary information about a vegetable eg. Shape, Color, Crate number and more
-
     Returns
     -------
     image_with_points : numpy array
@@ -226,16 +205,12 @@ def getpoint(pipeline1,pipeline2, vegetable):
     place : string
         Used to identify in which quadrant of the crate the vegetable is located
     '''
-
     crop=[[(20),(700),(600),(1075)],[(20),(680),(150),(600)],[(0),(680),(630),(1100)],[(0),(680),(170),(630)]]
-
     shape = vegetable["product_shape"]
     min_size = vegetable["product_minSize"]
     max_size = vegetable["product_maxSize"]
     hsv_range = vegetable["product_HSVRange"]
     crate_number = int(vegetable["crateNumber"])
-    CurrentHeighest=2000
-
     if (crate_number==1) or (crate_number==2):
         pipeline=pipeline1
         camera=1
@@ -243,32 +218,25 @@ def getpoint(pipeline1,pipeline2, vegetable):
         pipeline=pipeline2
         camera=2
     depth_cut,color_cut,orginal_color_frame=getframe(pipeline,crop[crate_number-1])
+    
     if (shape == "Round"):
-        image_with_points,pickup_coordinates,gray_image,angle,lenght=getpoint_round(color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
+        image_with_points,pickup_coordinates,gray_image=getpoint_round(depth_cut,color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
     elif (shape == "Not round"):
-        image_with_points,pickup_coordinates,gray_image,angle,lenght=getpoint_notround(color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
+        image_with_points,pickup_coordinates,gray_image=getpoint_notround(depth_cut,color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
     elif (shape == "Not round with stem"):
-        image_with_points,pickup_coordinates,gray_image,angle,lenght=getpoint_notround_withstem(color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
+        image_with_points,pickup_coordinates,gray_image=getpoint_notround_withstem(depth_cut,color_cut,hsv_range[0],hsv_range[1],hsv_range[2],hsv_range[3],hsv_range[4],hsv_range[5],min_size,max_size)
     #determine place in crate
-    for i in range(len(pickup_coordinates)):
-            depth_value = depth_cut[pickup_coordinates[i][0][1],pickup_coordinates[i][0][0]]
-            if depth_value<CurrentHeighest:
-                highest_coordinate=pickup_coordinates[i][0]
-                CurrentHeighest=depth_value
     if (pickup_coordinates != []):
-        if (highest_coordinate[1]<(crop[crate_number-1][1]-crop[crate_number-1][0])/2):
+        if (pickup_coordinates[0][0][1]<(crop[crate_number-1][1]-crop[crate_number-1][0])/2):
             place="Up"
-        elif(highest_coordinate[1]>=(crop[crate_number-1][1]-crop[crate_number-1][0])/2):
+        elif(pickup_coordinates[0][0][1]>=(crop[crate_number-1][1]-crop[crate_number-1][0])/2):
             place="Down"
     elif(pickup_coordinates == []):
-        highest_coordinate=[]
         place=None
     
-        
-    return image_with_points,highest_coordinate,gray_image,crop[crate_number-1],orginal_color_frame,camera,pipeline,place,angle,lenght
-
+    return image_with_points,pickup_coordinates,gray_image,crop[crate_number-1],orginal_color_frame,camera,pipeline,place
 def draw_original(original,coordinates,xcorrect=0,ycorrect=0):
-    cv2.circle(original,(coordinates[0]+xcorrect,coordinates[1]+ycorrect),1,(0,255,0),2)
+    cv2.circle(original,(coordinates[0][0][0]+xcorrect,coordinates[0][0][1]+ycorrect),1,(0,255,0),2)
     return original
 def initizalize_rs():
     # use the serial number of the camera to determine which camera is where
@@ -292,13 +260,11 @@ def initizalize_rs():
     #Start streaming
     cv2.waitKey(1000)
     return pipeline1,pipeline2
-
 def make_3D_point(x, y, pipeline, camera):
     '''
     Description
     ----------
     Function to calculate the coordinates of a point in the 3D space.
-
     Parameters
     ----------
     x : float
@@ -309,7 +275,6 @@ def make_3D_point(x, y, pipeline, camera):
         Camera pipeline object
     camera : int
         Integer value to specify in which camera frame the point needs to be calculated
-
     Returns
     -------
     point : array of float
@@ -322,7 +287,6 @@ def make_3D_point(x, y, pipeline, camera):
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-
         # Get the depth value at the point of interest
         depth_value = depth_frame.get_distance(x, y)
         # Convert depth pixel coordinates to world coordinates
@@ -339,20 +303,19 @@ def make_3D_point(x, y, pipeline, camera):
         xmean += world_coords[0]
         ymean += world_coords[1]
         zmean += world_coords[2]
-
     world_coords = np.array([xmean, ymean, zmean]) / number
     if camera == 1:
         cam1 = np.loadtxt('Cam_Off_1.txt')  # difference from the camera to the robot coordinates  HMI
         point = [-(world_coords[1]) + cam1[0], -(world_coords[0] ) + cam1[1],
                  (-world_coords[2] ) + cam1[2]]
-        slope=0.07
-        intercept=50
     elif camera == 2:
         cam2 = np.loadtxt('Cam_Off_2.txt')  # difference from the camera to the robot coordinates
         point = [-(world_coords[1]) + cam2[0], -(world_coords[0] ) + cam2[1],
                  (-world_coords[2] + cam2[2])]
-        slope=0.018
-        intercept=10
+    slope=0.05
+    intercept=40
+    slope=0.07
+    intercept=50
     correction=+(slope*point[0]+intercept)
     print(correction)
     point[2]=point[2]+correction
@@ -362,14 +325,12 @@ def calibrateXY(pipeline, robot_coordinates,camera):
     Description
     ----------
     Function to calculate the offset between the robot coordinates and camera coordinates.
-
     Parameters
     ----------
     pipeline : pyrealsense.pipeline
         Camera pipeline object
     robot_coordinates : array of float
         Array containing the coordinates of the robot position
-
     Returns
     -------
     x_offset : float
